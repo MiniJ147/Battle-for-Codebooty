6ba3785e6913cd2cc529f88c1f41a08b4340d530
aavillanuevaa <angelovill0908@gmail.com>
2024-10-05 05:12:43 -0400

    front page

--- frontend/src/pages/home/Page.jsx
+++ frontend/src/pages/home/Page.jsx
@@ -8,8 +8,13 @@ export const Home = () => {
       <div  className="bg-gradient-to-b from-soft-blue-start via-soft-blue-middle to-soft-blue-end min-h-96">
         <Header/>
       </div>
-      <div className='min-h-screen bg-wave-blue  m-0 p-0'>
-        <Wave/>
+      <Wave/>
+      <div className='relative w-full'>
+        <div className='absolute w-full h-10 bg-wave-blue bottom-0'>
+        </div>
+      </div>
+      <div className='min-h-screen bg-wave-blue'>
+          <h1 className='text-center py-10'>About Us</h1>
       </div>
     </>
   );
