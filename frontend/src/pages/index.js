579ad6ec6a96976e5472864a610a990bf6acd0c2
BooleanCube <samarthupadhya2309@gmail.com>
2024-10-05 01:11:41 -0400

    woopsie

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,6 @@
 import { Home } from './home/Page.jsx';
 import { Match } from './match/Page.jsx';
 import { Score } from './score/Page.jsx';
-import { NotFound } from './nopage/Page.jsx';
+import { NotFound } from './notfound/Page.jsx';
 
 export { Home, Match, Score, NotFound };
