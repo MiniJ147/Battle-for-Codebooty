6eea6fd6e7f7f4be9087ce17fcf5aec6abea8b25
avvooturi <114264497+avvooturi@users.noreply.github.com>
2024-10-06 02:08:25 -0400

    algorithms finished

--- backend/src/routes/actions.ts
+++ backend/src/routes/actions.ts
@@ -2,6 +2,13 @@ import express from "express";
 const router = express.Router();
 import assert from "assert";
 
+export function spyglass(code_sample: string): string {
+    if (!code_sample || code_sample.length === 0) {
+        return `Code is empty`;
+    }
+    return code_sample;
+}
+
 // Test cannon function, takes code sample and line number, and replaces characters with dots ('.')
 export function cannon(code_sample: string, lineNumber: number): string {
     if (!code_sample || code_sample.length === 0) {
@@ -24,6 +31,25 @@ export function cannon(code_sample: string, lineNumber: number): string {
     }
 
     return lines.join("\n");
+}
+
+export function raid(code_sample: string, lineNumber: number): string {
+    if (!code_sample || code_sample.length === 0) {
+        return `Code is empty`;
+    }
+
+    code_sample = code_sample.trim();
+
+    const lines = code_sample.split("\n");
+    if (lineNumber < 0 || lineNumber >= lines.length) {
+        return `Invalid line number ${lineNumber} and only has ${lines.length} lines`;
+    }
+
+    const endLine = Math.min(lineNumber + 10, lines.length);
+    const lineRange = lines.slice(lineNumber, endLine);
+    const selectedLines = lineRange;
+
+    return selectedLines.join("\n");
 }
 
 export default router;
